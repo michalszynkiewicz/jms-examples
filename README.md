31887b2565c224fa2685bd82f2e6205a112aeb1a
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2013-03-20 18:06:28 +0100

    beginning of readme

--- README.md
+++ README.md
@@ -3,7 +3,10 @@ mail-service
 
 Notification sender which reads messages from a queue.
 
-TODO: info about hornetq
+To run the application you need HornetQ server running with queue mailSenderQueue defined and JNDI enabled.
+You can customize JNDI settings and queue name in jms.properties file.
+If you use linux, you can user installHornetq.sh script to download and initialize the server.
+If you use this script, you'll
 
 To run either:
  - Run Server class in your favourite IDE
--- installHornetq.sh
+++ installHornetq.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 RUN_HORNET="export CLUSTER_PROPS=\"-Djnp.port=1099 -Djnp.rmiPort=1098 -Djnp.host=localhost -Dhornetq.remoting.netty.host=localhost -Dhornetq.remoting.netty.port=5445\"
 HORNETQ_DIR=\`dirname \"\${0}\"\`
 echo \"\${HORNETQ_DIR}/bin\"
@@ -19,5 +21,5 @@ chmod +x ${HORNET_RUN_SCRIPT}
 HORNET_JMS_CONFIGURATION="${HORNET_NAME}/config/stand-alone/non-clustered/hornetq-jms.xml"
 END_OF_CONFIGURATION="<\/configuration>"
 HORNET_JMS_CONF_TEMP_FILE="/tmp/hornetq-jms-`date`.xml"
-sed "s/${END_OF_CONFIGURATION}/${MAIL_QUEUE}\n&/g" ${HORNET_JMS_CONFIGURATION} > "${HORNET_JMS_CONF_TEMP_FILE}"
+sed "s/${END_OF_CONFIGURATION}/${MAIL_QUEUE}\n&/g" "${HORNET_JMS_CONFIGURATION}" > "${HORNET_JMS_CONF_TEMP_FILE}"
 mv "${HORNET_JMS_CONF_TEMP_FILE}" "${HORNET_JMS_CONFIGURATION}"
